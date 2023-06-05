6499409a6105460510a0f6ccecc95b43116c54ad
RauVR <u201714049@upc.edu.pe>
2023-06-05 16:42:55 -0500

    chore : Class StringExtensions Created

---
+++ BigFun.API/Shared/Extensions/StringExtensions.cs
@@ -0,0 +1,33 @@
+namespace BigFun.API.Shared.Extensions;
+
+public static class StringExtensions
+{
+    public static string ToSnakeCase(this string text)
+    {
+        
+        /*
+            Extending the functionality of string class
+         */
+        static IEnumerable<char> Convert(CharEnumerator e)
+        {
+            if (!e.MoveNext()) yield break;
+
+            yield return char.ToLower(e.Current);
+
+            while (e.MoveNext())
+            {
+                if (char.IsUpper(e.Current))
+                {
+                    yield return '_';
+                    yield return char.ToLower(e.Current);
+                }
+                else
+                {
+                    yield return e.Current;
+                }
+            }
+        }
+
+        return new string(Convert(text.GetEnumerator()).ToArray()); 
+    }
+}
