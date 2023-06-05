92546605409ba6dda2b5ccce6fde8c8230219365
RauVR <u201714049@upc.edu.pe>
2023-06-05 18:56:17 -0500

    refactor: Class AppDbContext refactorized

--- BigFun.API/Shared/Persistence/Contexts/AppDbContext.cs
+++ BigFun.API/Shared/Persistence/Contexts/AppDbContext.cs
@@ -1,3 +1,4 @@
+using BigFun.API.Booking.Domain.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace BigFun.API.Shared.Persistence.Contexts;
@@ -8,6 +9,11 @@ public class AppDbContext : DbContext
     public AppDbContext(DbContextOptions options) : base(options)
     {
     }
+    
+    
+    public DbSet<Attendee> Attendees { get; set; }
+    
+    
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
@@ -15,10 +21,15 @@ public class AppDbContext : DbContext
         
         
         //  ENTITIES
-        
-        
-        
-        
-        
+        builder.Entity<Attendee>().ToTable("Attendees");
+        builder.Entity<Attendee>().HasKey(p => p.Id);
+        builder.Entity<Attendee>().Property(p => p.Id).IsRequired().ValueGeneratedOnAdd();
+        builder.Entity<Attendee>().Property(p => p.UserName).IsRequired().HasMaxLength(30);
+
+        //builder.Entity<Attendee>
+
+
+
+
     }
 }
