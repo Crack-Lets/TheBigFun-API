d36b6e0b8c51a429893b9e37e4a814546f14ccaa
RauVR <u201714049@upc.edu.pe>
2023-06-05 18:57:36 -0500

    refactor: Class Attendees updated

--- BigFun.API/Booking/Domain/Models/Attendee.cs
+++ BigFun.API/Booking/Domain/Models/Attendee.cs
@@ -5,10 +5,9 @@ public class Attendee
     public int Id { get; set; }
     public string UserName{get; set;}
     public string Name { get; set; }
-    public string Password { get; set; }
-    
-    // Relationships
+    public string email { get; set; }
 
+    // Relationships
     public IList<Event> Events { get; set; } = new List<Event>();
     public IList<Payment> Payments { get; set; } = new List<Payment>();
 
