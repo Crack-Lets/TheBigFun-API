dc6946cc9005e6abb9c3d25eb3b64d903b9c561f
RauVR <u201714049@upc.edu.pe>
2023-06-05 17:09:00 -0500

    feat : Class Attendee created and new branch created

---
+++ BigFun.API/Booking/Domain/Models/Attendee.cs
@@ -0,0 +1,15 @@
+namespace BigFun.API.Booking.Domain.Models;
+
+public class Attendee
+{
+    public int Id { get; set; }
+    public string UserName{get; set;}
+    public string Name { get; set; }
+    public string Password { get; set; }
+    
+    // Relationships
+
+    public IList<Event> Events { get; set; } = new List<Event>();
+
+
+}
