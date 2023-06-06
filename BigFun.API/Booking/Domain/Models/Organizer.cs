5d02fc930801be3f1dfc4fa3d64995d50b961384
Isabella-SH <sorianoisabella28@gmail.com>
2023-06-05 19:35:16 -0500

    feat: Generation of the organizer object

--- BigFun.API/Booking/Domain/Models/Organizer.cs
+++ BigFun.API/Booking/Domain/Models/Organizer.cs
@@ -2,5 +2,17 @@ namespace BigFun.API.Booking.Domain.Models;
 
 public class Organizer
 {
-    
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string UserName { get; set; }
+    public string Email { get; set; }
+
+    // Relationships
+
+    public IList<Event> Events { get; set; } = new List<Event>();
+    public IList<Payment> Payments { get; set; } = new List<Payment>();
+
+    //esto en las clases de event y payment
+    //public int OrganizerId { get; set; }
+    //public Organizer Organizer { get; set; }
 }
