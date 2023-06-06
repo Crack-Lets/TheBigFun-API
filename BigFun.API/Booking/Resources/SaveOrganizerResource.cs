74aa6e6466f930388d507a85903f56082936cb86
Isabella-SH <sorianoisabella28@gmail.com>
2023-06-05 21:42:20 -0500

    feat:SaveOrganizerResource implemented

---
+++ BigFun.API/Booking/Resources/SaveOrganizerResource.cs
@@ -0,0 +1,8 @@
+namespace BigFun.API.Booking.Resources;
+
+public class SaveOrganizerResource
+{
+    public string Name { get; set; }
+    public string UserName { get; set; }
+    public string Email { get; set; }
+}
