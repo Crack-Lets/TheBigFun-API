760a99d9eeebf5921e132c427b227abb053df65b
RauVR <u201714049@upc.edu.pe>
2023-06-05 19:52:44 -0500

    feat: class SaveAttendeeResource Created

---
+++ BigFun.API/Booking/Resources/SaveAttendeeResource.cs
@@ -0,0 +1,8 @@
+namespace BigFun.API.Booking.Resources;
+
+public class SaveAttendeeResource
+{
+    public string UserName{get; set;}
+    public string Name { get; set; }
+    public string Email { get; set; } 
+}
