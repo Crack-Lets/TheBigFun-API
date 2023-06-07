9385de2f9a68b249b04d146b705a8b311f5bbf94
Ergotox <Diegojsf17@gmail.com>
2023-06-06 21:21:35 -0500

    feat: IPaymentService interface implemented

--- BigFun.API/Booking/Domain/Services/IPaymentService.cs
+++ BigFun.API/Booking/Domain/Services/IPaymentService.cs
@@ -1,7 +1,14 @@
+using BigFun.API.Booking.Domain.Models;
+using BigFun.API.Booking.Domain.Services.Communication;
+
 namespace BigFun.API.Booking.Domain.Services;
 
 public interface IPaymentService
 {
+    Task<IEnumerable<Payment>> ListAsync();
+    Task<PaymentResponse> SaveAsync(Payment payment);
+    Task<PaymentResponse> UpdateAsync(int id, Payment payment);
+    Task<PaymentResponse> DeleteAsync(int id);
     
-    
+
 }
