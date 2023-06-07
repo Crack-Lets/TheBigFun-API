913fd6a907a5e306b471acbe4f866ce666cb7845
Isabella-SH <sorianoisabella28@gmail.com>
2023-06-06 23:22:40 -0500

    feat: OrganizerEventsController and OrganizerPaymentsController created and implemented

---
+++ BigFun.API/Booking/Controllers/OrganizerEventsController.cs
@@ -0,0 +1,29 @@
+using AutoMapper;
+using BigFun.API.Booking.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BigFun.API.Booking.Controllers;
+
+[ApiController]
+[Route("/api/v1/organizers/{organizerId}/events")]
+public class OrganizerEventsController: ControllerBase
+{
+    private readonly IEventService _eventService;
+    private readonly IMapper _mapper;
+
+    public OrganizerEventsController(IEventService eventService, IMapper mapper)
+    {
+        _eventService = eventService;
+        _mapper = mapper;
+    }
+    
+    [HttpGet]
+    public async Task<IEnumerable<EventResource>> GetAllByOrganizerIdAsync(int organizerId)
+    {
+        var events = await _eventService.ListByOrganizerIdAsync(organizerId);
+
+        var resources = _mapper.Map<IEnumerable<Event>, IEnumerable<EventResource>>(events);
+
+        return resources;
+    }
+}
---
+++ BigFun.API/Booking/Controllers/OrganizerPaymentsController.cs
@@ -0,0 +1,57 @@
+using AutoMapper;
+using BigFun.API.Booking.Domain.Models;
+using BigFun.API.Booking.Domain.Services;
+using BigFun.API.Booking.Resources;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BigFun.API.Booking.Controllers;
+
+[ApiController]
+[Route("/api/v1/organizers/{organizerId}/payments")]
+public class OrganizerPaymentsController: ControllerBase
+{
+    private readonly IPaymentService _paymentService;
+    private readonly IMapper _mapper;
+
+    public OrganizerPaymentsController(IPaymentService paymentService, IMapper mapper)
+    {
+        _paymentService = paymentService;
+        _mapper = mapper;
+    }
+    
+    [HttpGet]
+    public async Task<IEnumerable<PaymentResource>> GetAllByOrganizerIdAsync(int organizerId)
+    {
+        var payments = await _paymentService.ListByOrganizerIdAsync(organizerId);
+
+        var resources = _mapper.Map<IEnumerable<Payment>, IEnumerable<PaymentResource>>(payments);
+
+        return resources;
+    }
+    
+    // booking-> services->PaymentService
+    /*
+     public async Task<IEnumerable<Payment>> ListByOrganizerIdAsync(int organizerId)
+     {
+        return await _paymentRepository.FindByOrganizerIdAsync(organizerId);
+      }
+     */
+    
+  // booking-> persistence->repositories->PaymentRepository
+     /*
+       public async Task<IEnumerable<Payment>> FindByOrganizerIdAsync(int organizerId)
+     {
+         return await _context.Payments
+             .Where(p => p.OrganizerId == organizerId)
+               .Include(p => p.Organizer)
+            .ToListAsync();
+    }
+     */
+    
+     // booking-> domain->services-> IPaymentService
+     /*
+        Task<IEnumerable<Payment>> ListByOrganizerIdAsync(int organizerId);
+
+     */
+     
+}
