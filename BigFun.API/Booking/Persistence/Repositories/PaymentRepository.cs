7fbd15e3097a85d725dcbadbf37b5238331dd35f
Ergotox <Diegojsf17@gmail.com>
2023-06-06 23:37:01 -0500

    feat: PaymentRepository class implemented

---
+++ BigFun.API/Booking/Persistence/Repositories/PaymentRepository.cs
@@ -0,0 +1,56 @@
+using System.Data.Entity;
+using System.Runtime.InteropServices.JavaScript;
+using BigFun.API.Booking.Domain.Models;
+using BigFun.API.Booking.Domain.Repositories;
+using BigFun.API.Shared.Persistence.Contexts;
+using BigFun.API.Shared.Persistence.Repositories;
+
+namespace BigFun.API.Booking.Persistence.Repositories;
+
+public class PaymentRepository : BaseRepository, IPaymentRepository
+{
+    public PaymentRepository(AppDbContext context) : base(context)
+    {
+    }
+
+    public async Task<IEnumerable<Payment>> ListAsync()
+    {
+        return await _context.Payments.ToListAsync();
+    }
+
+    public async Task AddAsync(Payment payment)
+    {
+        await _context.Payments.AddAsync(payment);
+    }
+
+    public async Task<Payment> FindByIdAsync(int id)
+    {
+        return await _context.Payments.FindAsync(id);
+    }
+
+    public async Task<Payment> FindByDateAsync(JSType.Date date)
+    {
+        return await _context.Payments.FindAsync(date);
+        
+    }
+
+    public Payment FindById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(Payment payment)
+    {
+        _context.Payments.Update(payment);
+    }
+
+    public void Remove(Payment payment)
+    {
+        _context.Payments.Remove(payment);
+    }
+
+    public async Task<IEnumerable<Payment>> FindByOrganizerIdAsync(int organizerId)
+    {
+        throw new NotImplementedException();
+    }
+}
