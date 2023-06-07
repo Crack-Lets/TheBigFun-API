e3dbb91884b85c31e2be5da06a5a2cee5a3d6eaa
RauVR <u201714049@upc.edu.pe>
2023-06-06 23:15:56 -0500

    refactor: Class SaveAttendeeResource updated

--- BigFun.API/Booking/Resources/SaveAttendeeResource.cs
+++ BigFun.API/Booking/Resources/SaveAttendeeResource.cs
@@ -1,8 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace BigFun.API.Booking.Resources;
 
 public class SaveAttendeeResource
 {
+    [Required]  
+    [MaxLength(50)]
     public string UserName{get; set;}
+    
+    [MaxLength(255)]
     public string Name { get; set; }
+    
+    [MaxLength(255)]
     public string Email { get; set; } 
 }
