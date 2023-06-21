80920ab3c6da46d5509ba87cc22c23c006fafd5b
RauVR <u201714049@upc.edu.pe>
2023-06-21 06:41:24 -0500

    refactor: PaymentController updated

--- BigFun.API/Booking/Controllers/PaymentController.cs
+++ BigFun.API/Booking/Controllers/PaymentController.cs
@@ -1,7 +1,9 @@
 using AutoMapper;
 using BigFun.API.Booking.Domain.Models;
+using BigFun.API.Booking.Domain.Repositories;
 using BigFun.API.Booking.Domain.Services;
 using BigFun.API.Booking.Resources;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BigFun.API.Booking.Controllers;
@@ -12,11 +14,13 @@ public class PaymentController
 {
     private readonly IPaymentService _paymentService;
     private readonly IMapper _mapper;
+    private readonly IEventRepository _eventRepository;
 
-    public PaymentController(IPaymentService paymentService, IMapper mapper)
+    public PaymentController(IPaymentService paymentService, IMapper mapper, IEventRepository eventRepository)
     {
         _paymentService = paymentService;
         _mapper = mapper;
+        _eventRepository = eventRepository;
     }
     
     [HttpGet]
@@ -30,6 +34,12 @@ public class PaymentController
     [HttpGet("{eventId}/payments")]
     public async Task<IEnumerable<PaymentResource>> GetPaymentsByEventIdAsync(int eventId)
     {
+        var events = await _eventRepository.FindByIdAsync(eventId);
+        if (events == null)
+        {
+            var errorMessage="the event doesn't exist";
+            throw new ArgumentException("the event doesn't exist");
+        }
         var paymentsByEvent = await _paymentService.ListByEventIdAsync(eventId);
         var resources = _mapper.Map<IEnumerable<Payment>, IEnumerable<PaymentResource>>(paymentsByEvent);
         return resources;
